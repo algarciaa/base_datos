317255dfa77ad0537d96f84124f30580c8ee29ad
algarciaa <al_garcia@tecnologicoargos.edu.ec>
2025-11-27 20:53:25 -0500

    Add multiple SQL queries for client and invoice data

--- selects_clases.sql
+++ selects_clases.sql
@@ -53,3 +53,109 @@ from tienda.factura f
      inner join tienda.producto p
      on df.id_producto = p.id_producto 
 where f.id_factura = 1;
+
+/*Deber*/
+
+select c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido,
+       f.id_factura,
+       f.creado_en
+from tienda.cliente c
+     inner join tienda.factura f
+     on c.id_cliente = f.id_cliente 
+where c.tipo_identificacion = 'RUC';
+
+select c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido,
+       f.id_factura,
+       p.nombre "nombre_producto"
+from tienda.cliente c
+     inner join tienda.factura f
+     on c.id_cliente = f.id_cliente
+     inner join tienda.detalle_factura df
+     on f.id_factura = df.id_factura
+     inner join tienda.producto p
+     on df.id_producto = p.id_producto 
+where c.tipo_identificacion = 'PASAPORTE';
+
+
+
+select c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido,
+       f.id_factura,
+       tp.nombre "nombre_tipo_producto"
+from tienda.cliente c
+     inner join tienda.factura f
+     on c.id_cliente = f.id_cliente
+     inner join tienda.detalle_factura df
+     on f.id_factura = df.id_factura
+     inner join tienda.producto p
+     on df.id_producto = p.id_producto 
+     inner join tienda.tipo_producto tp
+     on p.id_tipo_producto = tp.id_tipo_producto 
+where c.tipo_identificacion = 'CEDULA';
+
+/*Clase 27/11/2025*/
+--contar las facturas por cliente
+select c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido,
+       count(f.id_factura) total_factura
+from tienda.cliente c
+     inner join tienda.factura f
+     on c.id_cliente = f.id_cliente 
+where c.tipo_identificacion = 'RUC'
+group by c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido;
+
+select distinct
+       c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido,
+       p.nombre "nombre_producto"
+from tienda.cliente c
+     inner join tienda.factura f
+     on c.id_cliente = f.id_cliente
+     inner join tienda.detalle_factura df
+     on f.id_factura = df.id_factura
+     inner join tienda.producto p
+     on df.id_producto = p.id_producto 
+where c.tipo_identificacion = 'PASAPORTE'
+--order by c.id_cliente, p.nombre;
+order by 1, 6;
+
+select distinct
+       c.id_cliente,
+       c.tipo_identificacion,
+       c.identificacion,
+       c.nombre,
+       c.apellido,
+       tp.nombre "nombre_tipo_producto"
+from tienda.cliente c
+     inner join tienda.factura f
+     on c.id_cliente = f.id_cliente
+     inner join tienda.detalle_factura df
+     on f.id_factura = df.id_factura
+     inner join tienda.producto p
+     on df.id_producto = p.id_producto 
+     inner join tienda.tipo_producto tp
+     on p.id_tipo_producto = tp.id_tipo_producto 
+where c.tipo_identificacion = 'CEDULA';
+
+
